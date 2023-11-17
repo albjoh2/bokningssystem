c5fb4ad3578886b2e6906c663c6bedc775097457
albjoh <aljo940616@gmail.com>
2023-11-17 14:02:10 +0100

    Connect to API

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ function validate() {
   }
   if (validated === true) {
     newName();
+    storeCustomer();
   }
 }
 
@@ -147,3 +148,132 @@ function closedialog() {
 $(function () {
   $("#accordion").accordion();
 });
+
+let apptype = "a22albjo";
+
+function fixChars(returnedData) {
+  var resultset = returnedData.childNodes[0];
+
+  // Iterate over all nodes in root node recursively and replace the strings inside attributes
+  x = returnedData.getElementsByTagName("*");
+  for (i = 0; i < x.length; i++) {
+    for (j = 0; j < x[i].attributes.length; j++) {
+      x[i].attributes[j].nodeValue = x[i].attributes[j].nodeValue.replace(
+        /%/g,
+        "&"
+      );
+    }
+  }
+}
+
+function storeCustomer() {
+  var input = {
+    ID: `a22albjo${Math.random()}`,
+    firstname: document.getElementById("customerfnameC").value,
+    lastname: document.getElementById("customerlnameC").value,
+    email: document.getElementById("email").value,
+    address: document.getElementById("customeraddressC").value,
+  };
+  console.log(input);
+
+  fetch("./booking/makecustomer_XML.php", {
+    method: "POST", // or 'PUT'
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(input),
+  })
+    .then(function (response) {
+      // first then()
+      if (response.ok) return response.text();
+      throw new Error(response.statusText);
+    })
+    .then(function (text) {
+      ResultBookingCustomer(
+        new window.DOMParser().parseFromString(text, "text/xml")
+      );
+    })
+    .catch(function (error) {
+      // catch
+      alert("Request failed\n" + error);
+    });
+}
+
+function ResultCustomern(returnedData) {
+  // Iterate over all nodes in root node (i.e. the 'created' element in root which has an attribute called status)
+  for (i = 0; i < returnedData.childNodes.length; i++) {
+    if (returnedData.childNodes.item(i).nodeName == "created") {
+      alert(returnedData.childNodes.item(i).attributes["status"].value);
+    }
+  }
+}
+
+//------------------------------------------------------------------------
+// search resources
+//------------------------------------------------------------------------
+// Searches through the resources for a certain application.
+// If only type is given all resources for application are given
+// If either company, location or name are given in any combination, these values are searched
+// If fulltext is given all attributes are searched at once
+//------------------------------------------------------------------------
+
+function searchResources() {
+  var input = {
+    type: apptype,
+    name: document.getElementById("resNameS").value,
+    location: document.getElementById("resLocationS").value,
+    company: document.getElementById("resCompanyS").value,
+    fulltext: document.getElementById("resFulltextS").value,
+    id: document.getElementById("resIDS").value,
+  };
+
+  fetch("./booking/getresources_XML.php", {
+    method: "POST", // or 'PUT'
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(input),
+  })
+    .then(function (response) {
+      // first then()
+      if (response.ok) return response.text();
+      throw new Error(response.statusText);
+    })
+    .then(function (text) {
+      showResources(new window.DOMParser().parseFromString(text, "text/xml"));
+    })
+    .catch(function (error) {
+      // catch
+      alert("Request failed\n" + error);
+    });
+}
+
+function showResources(returnedData) {
+  console.log(returnedData);
+  // Fix characters in XML notation to HTML notation
+  fixChars(returnedData);
+
+  // An XML DOM document is returned from AJAX
+  var resultset = returnedData.childNodes[0];
+  var output = "";
+  // Iterate over all nodes in root node (i.e. resources)
+  for (i = 0; i < resultset.childNodes.length; i++) {
+    // Iterate over all child nodes of that node that are resource nodes
+    if (resultset.childNodes.item(i).nodeName == "resource") {
+      // Retrieve data from resource nodes
+      var resource = resultset.childNodes.item(i);
+      output += "<h3>Room 1</h3><div class='accordion' >";
+      output +=
+        "<p><b>Size</b>" + resource.attributes["size"].value + "kvm</p>";
+      output +=
+        "<p><b>Size</b>" + resource.attributes["company"].value + "kvm</p>";
+      output +=
+        "<p><b>Size</b>" + resource.attributes["name"].value + "kvm</p>";
+      output +=
+        "<p><b>Size</b>" + resource.attributes["location"].value + "kvm</p>";
+      output +=
+        "<p><b>Size</b>" + resource.attributes["category"].value + "kvm</p>";
+
+      output += "</div>";
+    }
+  }
+
+  var div = document.querySelector(".OutputDivSearchR");
+  div.innerHTML = output;
+}
